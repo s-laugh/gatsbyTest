d5c53dcd9c27e46b2af9541c6542fe8fdbae771d
s.laugh <shaun.laughland@hrsdc-rhdcc.gc.ca>
2019-07-12 12:17:06 -0400

    cleaning files for new project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,55 +1,12 @@
 import React from "react"
 import Layout from "../components/layout"
-import { graphql, Link } from "gatsby"
-import { css } from "@emotion/core"
-import { rhythm } from "../utils/typography"
 
 export default ({ data }) => {
   console.log(data)
   return (
     <Layout>
-      <h1>Amazing Pandas Eating Things</h1>
-      <div>
-        <img
-          src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
-          alt="Group of pandas eating bamboo"
-        />
-      </div>
-      <h2>{data.allMarkdownRemark.totalCount} Posts</h2>
-      {data.allMarkdownRemark.edges.map(({node}) => (
-        <div key={node.id}>
-          <Link to={node.fields.slug} css={css`text-decoration:none;color:inherit;`}>
-          <h3 css={css`margin-bottom:${rhythm(1/4)};`}>
-            {node.frontmatter.title}
-            <span css={css`color:#bbb;`}>
-              - {node.frontmatter.date}
-            </span>
-          </h3>
-          <p>{node.excerpt}</p>
-          </Link>
-        </div>
-      ))}
+      <h1>Dashboard Home</h1>
+      <p>Todo, add data dump from trello.</p>
     </Layout>
   )
 }
-
-export const query = graphql`
-  query {
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      totalCount
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            date(formatString: "DD MMMM, YYYY")
-          }
-          fields {
-            slug
-          }
-          excerpt
-        }
-      }
-    }
-  }
-`
